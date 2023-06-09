36949acf274368af52bfd3e85be936084b063b10
Cesar Augusto <peixoto.cesar@hotmail.com>
2023-06-09 01:08:37 -0300

    adicionado o arquivo etapas.js que funciona como um JSON e Iniciado a parte de JS para funcionamento da Urna.

---
+++ assets/js/script.js
@@ -0,0 +1,90 @@
+// Controla a interface
+let seuVotoPara = document.querySelector('.d1_area_1 span');
+let cargo = document.querySelector('.d1_area_2 span');
+let descricao = document.querySelector('.d1_area_4');
+let aviso = document.querySelector('.d2');
+let lateral = document.querySelector('.d1_right');
+let numeros = document.querySelector('.d1_area_3');
+
+let etapaAtual = 0;
+let numero = '';
+
+function comecarEtapa() {
+    let etapa = etapas[etapaAtual];
+
+    let numeroHTML = '';
+
+    for(let i=0; i < etapa.numeros; i++) {
+        if(i === 0) {
+            numeroHTML += '<div class="numero pisca"></div>';
+        } else {
+            numeroHTML += '<div class="numero"></div>';
+        } 
+    }
+
+    seuVotoPara.style.display = 'none';
+    cargo.innerHTML = etapa.titulo;
+    descricao.innerHTML = '';
+    aviso.style.display = 'none';
+    lateral.innerHTML = '';
+    numeros.innerHTML = numeroHTML;
+}
+
+
+function atualizaInterface() {
+    let etapa = etapas[etapaAtual];
+    let candidato = etapa.candidatos.filter((item)=>{
+        if(item.numero === numero) {
+            return true;
+        } else {
+            return false;
+        }
+    });
+
+    if(candidato.length > 0) {
+        candidato = candidato[0];
+        seuVotoPara.style.display = 'block';
+        aviso.style.display = 'block';
+        descricao.innerHTML = `Nome: ${candidato.nome}<br/>Partido: ${candidato.partido}`;
+           
+        let fotosHtml = ''; 
+        for(let i in candidato.fotos) {
+            fotosHtml += `<div class="d1_imagem"><img src="images/${candidato.fotos[i].url}" alt="">${candidato.fotos[i].legenda}</div>`
+        }
+
+        lateral.innerHTML = fotosHtml;
+    } else {
+        seuVotoPara.style.display = 'block';
+        aviso.style.display = 'block';
+        descricao.innerHTML = '<div class="aviso_grande pisca">VOTO NULO</div>';
+    }
+}
+
+function clicou(n) {
+    let elNumero = document.querySelector('.numero.pisca');
+    if(elNumero !== null) {
+        elNumero.innerHTML = n;
+        numero = `${numero}${n}`;
+
+        elNumero.classList.remove('pisca');
+        if(elNumero.nextElementSibling !== null) {
+            elNumero.nextElementSibling.classList.add('pisca');
+        } else {
+            atualizaInterface();
+        }
+    }
+}
+
+function branco() {
+
+}
+
+function corrige() {
+
+}
+
+function confirma() {
+
+}
+
+comecarEtapa();
